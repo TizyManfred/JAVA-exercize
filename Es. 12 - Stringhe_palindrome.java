e35fb1facf9e404fb8fe1c43d6e29ac2ec59a51e
TizyManfred <97536032+TizyManfred@users.noreply.github.com>
2022-03-08 11:23:43 +0100

    Update Es. 12 - Stringhe_palindrome.java

--- Es. 12 - Stringhe_palindrome.java
+++ Es. 12 - Stringhe_palindrome.java
@@ -1,1 +1,29 @@
-.
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int lunghezza, i;
+        String originale, reverse = ""; // Objects of String class
+
+        Scanner input;
+        input = new Scanner(System.in);
+
+        System.out.print("Inserisci una parola o una frase: ");
+        originale = input.nextLine();
+
+        lunghezza = originale.length();
+        i = lunghezza - 1;
+
+        for ( ; i >= 0; i-- )
+            reverse = reverse + originale.charAt(i);
+            if (originale.equals(reverse)) {
+                System.out.println("La stringa è palindroma");
+            }
+            else {
+                System.out.println("La stringa non è palindroma");
+            }
+    }
+}
